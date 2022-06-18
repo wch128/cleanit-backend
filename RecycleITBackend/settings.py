80bf82adb950e9b8ddf52861c3c0b9e0a8e51a11
Waqas Chaudhary <wchofficials128@gmail.com>
2022-06-19 01:56:01 +0500

    1st commit

--- RecycleITBackend/settings.py
+++ RecycleITBackend/settings.py
@@ -111,6 +111,7 @@ DATABASE_URL = 'postgres://kmziqrjgwyajvy:dba72c2fdcc4e0c3ed62c52865a42309741b9f
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES = {'default': dj_database_url.config(default=DATABASE_URL ) }
 
 #DATABASES = {
