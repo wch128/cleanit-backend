9a1bce22cfbcd12ddc7a02bf12b809cb9c1cba9a
Waqas Chaudhary <wchofficials128@gmail.com>
2022-06-19 15:14:06 +0500

    1st commit

--- RecycleITBackend/settings.py
+++ RecycleITBackend/settings.py
@@ -191,7 +191,5 @@ STATIC_URL = '/static/'
 #location where django collect all static files
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # location where you will store your static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'project_name/static')
-]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
